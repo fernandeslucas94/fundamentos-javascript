80d0848fcf6bde929a0f7fe755f2be89f1571a20
Lucas Fernandes <lucasfeitosa8@gmail.com>
2022-11-10 09:40:09 -0300

    Inserido conteudo sobre tipos de dados

---
+++ Conceitos Básicos/tiposDeDados.js
@@ -0,0 +1,10 @@
+/// Nesta seção de estudos, iremos verificar sobre os tipos de dados e suas particularidades.
+//
+/// O que são tipos de dados ? Como o próprio nome já diz, são maneiras que podemos identificar e categorizar alguma informação. Temos vários tipos de dados, e veremos aqui os que mais utilizamos no Javascript.
+//
+/// Podemos classificar os tipos de dados em: 
+//
+// String = texto
+// Number = números que sejam inteiros, ou seja, não possuem casa decimal (1, 2, 50, 100, etc...)
+// Float = números que podem possuir casas decimas (1.5, 2.7, 50.3, 103.9, etc...)
+// Boolean = com o tipo boolean, ou bool em algumas linguagens, podemos ter apenas dois resultados: true/verdadeiro ou false/falso. Utilizamos o boolean para determinarmos se alguma informação é verdadeira ou falsa, como por exemplo, em um caso de validação de formulário ou login.
